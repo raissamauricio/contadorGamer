79f868b57a1f00e12aa8d86cee90be07f2402b56
raissamauricio <raissaamauriciooo@gmail.com>
2026-05-14 10:05:42 -0300

    imp. estilo

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, {useState} from "react";
-import {View, Text, Button, StyleSheet } from "react-native-web";
+import {
+  View,
+  Text, 
+  Button, 
+  StyleSheet,
+  TouchableOpacity
+ } from "react-native-web";
+
+
 
 export default function App(){
   const [pontos, setPontos] = useState (0);
@@ -17,16 +25,79 @@ export default function App(){
   }
 
   return(
-    <View>
-      <Text>
-        🎮 Contador Gamer
+    <View style={styles.container}>
+      <Text style={styles.titulo}>
+        🎮 Contador Gamer 😛
       </Text>
-      <Text>{pontos}</Text>
-      <View>
-        <Button title="+1" onPress={aumentar}/>
-        <Button title="-1" onPress={diminuir}/>
-        <Button title="Reset"onPress={resetar}/>
+      <Text style={styles.pontos}>{pontos}</Text>
+      <View style={styles.areaBotoes}>
+       <TouchableOpacity
+        onPress={aumentar}
+       style={styles.botao}>
+        <Text style={styles.textoBotao}>+1</Text>
+       </TouchableOpacity>
+
+         <TouchableOpacity
+        onPress={diminuir}
+       style={styles.botao}>
+        <Text style={styles.textoBotao}>-1</Text>
+       </TouchableOpacity>
       </View>
+      <TouchableOpacity
+        onPress={resetar}
+       style={styles.botaoReset}>
+        <Text style={styles.textoBotao}>resetar</Text>
+       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  titulo: {
+    fontSize: 32,
+    color: '#00ff88',
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+
+  pontos: {
+    fontSize: 80,
+    color: '#ffffff',
+    marginBottom: 40,
+    fontWeight: 'bold',
+  },
+
+  areaBotoes: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+
+  botao: {
+    backgroundColor: '#00ff88',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 12,
+    marginHorizontal: 10,
+  },
+
+  botaoReset: {
+    backgroundColor: '#ff3b30',
+    paddingVertical: 15,
+    paddingHorizontal: 40,
+    borderRadius: 12,
+  },
+
+  textoBotao: {
+    fontSize: 22,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+});
